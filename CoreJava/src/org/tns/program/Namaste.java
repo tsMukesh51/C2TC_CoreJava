a151d94899c7a40fd3f547c40f49787c6010bae9
mukesh <mukesh@RANGA-LM>
2022-10-23 10:54:08 +0530

    Program

---
+++ CoreJava/src/org/tns/program/Namaste.java
@@ -0,0 +1,10 @@
+package org.tns.program;
+
+public class Namaste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Namaste World!");
+	}
+
+}
