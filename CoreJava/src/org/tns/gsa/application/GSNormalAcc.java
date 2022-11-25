6b823556d8d0a24c8e3510e090ca37f55e3499a9
mukesh <mukesh@RANGA-LM>
2022-11-25 10:26:19 +0530

    GSA Update

--- CoreJava/src/org/tns/gsa/application/GSNormalAcc.java
+++ CoreJava/src/org/tns/gsa/application/GSNormalAcc.java
@@ -3,14 +3,14 @@ package org.tns.gsa.application;
 import org.tns.gsa.framework.NormalAcc;
 
 public class GSNormalAcc extends NormalAcc{
-	public GSNormalAcc(int AccNo, String accNm, float charges, float deliveryCharges){
-		super(AccNo, accNm, charges, deliveryCharges);
+	public GSNormalAcc(int accNo, String accNm, float charges, float deliveryCharges){
+		super(accNo, accNm, charges, deliveryCharges);
 	}
 	public void bookProduct(float charges) {
 		System.out.println("Your total payable amount is: "+ charges);
 	}
 	public String toString() {
-		return ("Name: " + super.getAccNm() + "\nAccount Number" + super.getAccNo() + "Total Charges" + (super.getDeliveryCharges() + super.getCharges()));
+		return ("Name: " + getAccNm() + "\nAccount Number" + super.getAccNo() + "Total Charges" + (super.getDeliveryCharges() + super.getCharges()));
 		}
 	@Override
 	public void items(float item) {
